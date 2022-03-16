4082ef8a728be89dbbf2041d180e3265835beaae
cassandra-mn <arthenciac@gmail.com>
2022-03-16 00:42:13 -0300

    layout da tela 1

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import Tela1 from "./Tela1";
+
+export default function App() {
+    return (
+        <div>
+            <Tela1 />
+        </div>
+    );
+}
---
+++ src/Tela1.js
@@ -0,0 +1,11 @@
+export default function Tela1() {
+    return (
+        <div class="tela1">
+            <div class="container">
+                <img src="assets/image 1.png" alt="raio" />
+                <h1>ZapRecall</h1> 
+            </div>
+            <button><p>Iniciar Recall!</p></button>
+        </div>
+    );
+}
