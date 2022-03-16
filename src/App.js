5a9586df36c02c59ba4a102104ee9c74d060c78c
cassandra-mn <arthenciac@gmail.com>
2022-03-16 02:12:00 -0300

    layout tela 2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Tela1 from "./Tela1";
+import Tela2 from "./Tela2";
 
 export default function App() {
     return (
         <div>
-            <Tela1 />
+            {/* <Tela1 /> */}
+            <Tela2 />
         </div>
     );
 }
---
+++ src/Tela2.js
@@ -0,0 +1,40 @@
+const perguntas = [
+    { pergunta: "O que é JSX?", resposta: "Uma extensão de linguagem do JavaScript" },
+    { pergunta: "O React é __", resposta: "uma biblioteca JavaScript para construção de interfaces" },
+    { pergunta: "Componentes devem iniciar com __", resposta: "letra maiúscula" },
+    { pergunta: "Podemos colocar __ dentro do JSX", resposta: "expressões" },
+    { pergunta: "O ReactDOM nos ajuda __", resposta: "interagindo com a DOM para colocar componentes React na mesma" },
+    { pergunta: "Usamos o npm para __", resposta: "gerenciar os pacotes necessários e suas dependências" },
+    { pergunta: "Usamos props para __", resposta: "passar diferentes informações para componentes" },
+    { pergunta: "Usamos estado (state) para __", resposta: "dizer para o React quais informações quando atualizadas devem renderizar a tela novamente" }
+];
+
+console.log(perguntas.length);
+
+export default function Tela2() {
+    return (
+        <div class="tela2">
+            <header>
+                <img src="assets/image 1.png" alt="raio" />
+                <h1>ZapRecall</h1>
+            </header>
+            <main>
+                {perguntas.map((pergunta, index) =>
+                    <Perguntas index={index+1} />    
+                )}
+            </main>
+            <footer>
+                <p>0/8 CONCLUÍDOS</p>
+            </footer>
+        </div>
+    );
+}
+
+function Perguntas(props) {
+    return (
+        <div class="perguntas">
+            <p>Pergunta {props.index}</p>
+            <img src="assets/vetor.svg" />
+        </div>
+    );
+}
