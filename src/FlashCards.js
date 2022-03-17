cad861e0b434ca82f083776372138076a0e721f4
cassandra-mn <arthenciac@gmail.com>
2022-03-17 17:19:17 -0300

    mostrando as perguntas

--- src/FlashCards.js
+++ src/FlashCards.js
@@ -1,10 +1,21 @@
 import React from "react";
+import Decks from "./Decks";
+
+let i = null;
 
 export default function FlashCards(props) {
-    return (
-        <div className="perguntas">
-            <p>Pergunta {props.index}</p>
-            <img src="assets/vetor.svg" />
-        </div>
-    );
+    const [esconder, setEsconder] = React.useState(true);
+
+    if (esconder) {
+        return (
+            <div className="perguntas" onClick={() => { 
+                    setEsconder(false);
+                    i = props.index - 1;
+                }}>
+                <p>Pergunta {props.index}</p>
+                <img src="assets/vetor.svg" />
+            </div>
+        );
+    }
+    return <Decks index={i} />;
 }
