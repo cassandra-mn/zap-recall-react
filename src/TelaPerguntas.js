ed4bb09bb59642243a2be271392fb306f7c9e7af
cassandra-mn <arthenciac@gmail.com>
2022-03-18 15:23:24 -0300

    botão de reiniciar

--- src/TelaPerguntas.js
+++ src/TelaPerguntas.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Decks from "./Decks";
+import TelaInicial from "./TelaInicial";
 
 export default function TelaPerguntas() {
     const flashcards = [
@@ -15,13 +16,14 @@ export default function TelaPerguntas() {
     const [concluidos, setConcluidos] = React.useState(0);
     const [emojis, setEmojis] = React.useState([]);
     const [incorretas, setIncorretas] = React.useState(0);
+    const [visivel, setVisivel] = React.useState(true);
     const imgFinal = incorretas === 0 ? "assets/party.svg" : "assets/sad.svg";
     const h1Final = incorretas === 0 ? "PARABÉNS!" : "PUTZ!";
     const pFinal = incorretas === 0 ? "Você não esqueceu de nenhum flashcard!" : "Ainda faltaram alguns... Mas não desanime!";
 
     function embaralhar(array) {
         for (let i = array.length - 1; i > 0; i--) {
-            const j = Math.floor(Math.random() * (i+1));
+            const j = Math.floor(Math.random() * (i + 1));
             [array[i], array[j]] = [array[j], array[i]];
         }
         return array;
@@ -29,49 +31,53 @@ export default function TelaPerguntas() {
 
     const flashcardsEmbaralhados = embaralhar(flashcards);
 
-    return concluidos < flashcards.length ? (
-        <div className="telaPerguntas">
-            <header>
-                <img src="assets/raio.png" alt="raio" />
-                <h1>ZapRecall</h1>
-            </header>
-            <main>
-                {flashcardsEmbaralhados.map((card, index) =>
-                    <Decks key={index} index={index} flashcards={flashcardsEmbaralhados} concluidos={valor => setConcluidos(concluidos+valor)} emojis={resposta => setEmojis([...emojis, resposta])} incorretas={quantidade => setIncorretas(incorretas+quantidade)} />    
-                )}
-            </main>
-            <footer>
-                <p>{concluidos}/8 CONCLUÍDOS</p>
-                <div className="emojis">
-                    {emojis.map((emoji, index) => {
-                        return <img key={index} src={emoji} alt={emoji} />
-                    })}
-                </div>
-            </footer>
-        </div>
-    ) : (
-        <div className="telaPerguntas">
-            <header>
-                <img src="assets/raio.png" alt="raio" />
-                <h1>ZapRecall</h1>
-            </header>
-            <main>
-                {flashcards.map((card, index) =>
-                    <Decks key={index} index={index} flashcards={flashcardsEmbaralhados} concluidos={valor => setConcluidos(concluidos+valor)} emojis={resposta => setEmojis([...emojis, resposta])} />    
-                )}
-            </main>
-            <footer className="parte-final">
-                <div className="alinhar">
-                    <img src={imgFinal} alt="party" />
-                    <h1>{h1Final}</h1>
-                </div>
-                <p>{pFinal}</p>
-                <div className="emojis">
-                    {emojis.map((emoji, index) => {
-                        return <img key={index} src={emoji} alt={emoji} />
-                    })}
-                </div>
-            </footer>
-        </div>
-    );
+    if (visivel) {
+        return concluidos < flashcards.length ? (
+            <div className="telaPerguntas">
+                <header>
+                    <img src="assets/raio.png" alt="raio" />
+                    <h1>ZapRecall</h1>
+                </header>
+                <main>
+                    {flashcardsEmbaralhados.map((card, index) =>
+                        <Decks key={index} index={index} flashcards={flashcardsEmbaralhados} concluidos={valor => setConcluidos(concluidos + valor)} emojis={resposta => setEmojis([...emojis, resposta])} incorretas={quantidade => setIncorretas(incorretas + quantidade)} />
+                    )}
+                </main>
+                <footer>
+                    <p>{concluidos}/8 CONCLUÍDOS</p>
+                    <div className="emojis">
+                        {emojis.map((emoji, index) => {
+                            return <img key={index} src={emoji} alt={emoji} />
+                        })}
+                    </div>
+                </footer>
+            </div>
+        ) : (
+            <div className="telaPerguntas">
+                <header>
+                    <img src="assets/raio.png" alt="raio" />
+                    <h1>ZapRecall</h1>
+                </header>
+                <main>
+                    {flashcards.map((card, index) =>
+                        <Decks key={index} index={index} flashcards={flashcardsEmbaralhados} concluidos={valor => setConcluidos(concluidos + valor)} emojis={resposta => setEmojis([...emojis, resposta])} />
+                    )}
+                </main>
+                <footer className="parte-final">
+                    <div className="alinhar">
+                        <img src={imgFinal} alt="party" />
+                        <h1>{h1Final}</h1>
+                    </div>
+                    <p>{pFinal}</p>
+                    <div className="emojis">
+                        {emojis.map((emoji, index) => {
+                            return <img key={index} src={emoji} alt={emoji} />
+                        })}
+                    </div>
+                    <button onClick={() => setVisivel(false)}><p>REINICIAR RECALL</p></button>
+                </footer>
+            </div>
+        );
+    }
+    return <TelaInicial />;
 }
