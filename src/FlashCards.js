41f8ef24a24c7bfdc875e970b67e9b8f007cf116
cassandra-mn <arthenciac@gmail.com>
2022-03-17 21:45:50 -0300

    o card vira e mostra a resposta

--- src/FlashCards.js
+++ src/FlashCards.js
@@ -1,10 +1,18 @@
+import React from "react";
+
 export default function FlashCards(props) {
     const { flashcards, index } = props;
+    const [virada, setVirada] = React.useState(true);
 
-    return (
+    return virada ? (
         <div className="pergunta-flash">
             <p>{flashcards[index].pergunta}</p>
-            <img src="/assets/Vector.svg"></img>
+            <img src="/assets/Vector.svg" onClick={() => setVirada(!virada)}></img>
         </div>
-    );
+    )
+        : (
+            <div className="pergunta-flash">
+                <p>{flashcards[index].resposta}</p>
+            </div>
+        );
 }
