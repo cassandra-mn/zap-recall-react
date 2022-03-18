4f408fa9d9fbc36f8ab75ed2daf7fd88a0484420
cassandra-mn <arthenciac@gmail.com>
2022-03-17 22:51:44 -0300

    layout tela 4

--- src/FlashCards.js
+++ src/FlashCards.js
@@ -5,14 +5,19 @@ export default function FlashCards(props) {
     const [virada, setVirada] = React.useState(true);
 
     return virada ? (
-        <div className="pergunta-flash">
+        <div className="cards">
             <p>{flashcards[index].pergunta}</p>
             <img src="/assets/Vector.svg" onClick={() => setVirada(!virada)}></img>
         </div>
     )
         : (
-            <div className="pergunta-flash">
+            <div className="cards">
                 <p>{flashcards[index].resposta}</p>
+                <div className="caixinhas">
+                    <div className="caixinha vermelha"><p>Não lembrei</p></div>
+                    <div className="caixinha laranja"><p>Quase não lembrei</p></div>
+                    <div className="caixinha verde"><p>Zap!</p></div>
+                </div>
             </div>
         );
 }
