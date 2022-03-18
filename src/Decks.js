73b546a1c6978129440f343b62229f8056d32b2c
cassandra-mn <arthenciac@gmail.com>
2022-03-18 00:31:50 -0300

    agora com os emojis

--- src/Decks.js
+++ src/Decks.js
@@ -5,16 +5,20 @@ export default function Decks(props) {
     const { flashcards, index } = props;
     const [esconder, setEsconder] = React.useState(true);
     const [classe, setClasse] = React.useState("");
+    const [emoji, setEmoji] = React.useState("assets/vetor.svg");
 
     function mudarEstado(status) {
         setEsconder(!esconder);
         setClasse(status);
+        if (status === "correto") setEmoji("assets/correto.svg");
+        if (status === "incorreto") setEmoji("assets/incorreto.svg");
+        if (status === "meio-termo") setEmoji("assets/meio-termo.svg");
     }
 
     return esconder ?
         (<div className="perguntas" onClick={mudarEstado}>
             <p className={classe}><strong>Pergunta {props.index+1}</strong></p>
-            <img src="assets/vetor.svg" />
+            <img src={emoji} alt={classe}/>
         </div>)
         : <FlashCards index={index} flashcards={flashcards} mudarEstado={mudarEstado} />;
 }
--- src/FlashCards.js
+++ src/FlashCards.js
@@ -4,20 +4,17 @@ export default function FlashCards(props) {
     const {flashcards, index, mudarEstado} = props;
     const [virada, setVirada] = React.useState(true);
 
-    return virada ? (
-        <div className="cards">
+    return virada ? 
+        (<div className="cards">
             <p>{flashcards[index].pergunta}</p>
-            <img src="/assets/Vector.svg" onClick={() => setVirada(!virada)}></img>
-        </div>
-    )
-        : (
-            <div className="cards">
-                <p>{flashcards[index].resposta}</p>
-                <div className="caixinhas">
-                    <div className="caixinha vermelha" onClick={() => mudarEstado("incorreto")}><p>Não lembrei</p></div>
-                    <div className="caixinha laranja" onClick={() => mudarEstado("meio-termo")}><p>Quase não lembrei</p></div>
-                    <div className="caixinha verde" onClick={() => mudarEstado("correto")}><p>Zap!</p></div>
-                </div>
+            <img src="/assets/Vector.svg" onClick={() => setVirada(!virada)} alt="seta" />
+        </div>) : 
+        (<div className="cards">
+            <p>{flashcards[index].resposta}</p>
+            <div className="caixinhas">
+                <div className="caixinha vermelha" onClick={() => mudarEstado("incorreto")}><p>Não lembrei</p></div>
+                <div className="caixinha laranja" onClick={() => mudarEstado("meio-termo")}><p>Quase não lembrei</p></div>
+                <div className="caixinha verde" onClick={() => mudarEstado("correto")}><p>Zap!</p></div>
             </div>
-        );
+        </div>);
 }
